15c905bdb037f50d938f94852c3cca31190cda16
Natanael Freire <bsbinfor@gmail.com>
2022-01-19 19:58:35 -0300

    fix: lint

--- src/singleton/app.ts
+++ src/singleton/app.ts
@@ -3,6 +3,7 @@ import Singleton from './singleton'
 const app = () => {
   const singleton1 = Singleton.getInstance()
   const singleton2 = Singleton.getInstance()
+
   console.log('Singleton 1: ', singleton1.getUuid())
   console.log('Singleton 2: ', singleton2.getUuid())
 }
